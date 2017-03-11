9c3ff06b32c380c869a9497a15ede8b4e36e2ee2
Ain <ain.salula@gmail.com>
2017-03-11 22:52:01 +0200

    configure routing

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -4,5 +4,19 @@ import { render } from 'react-dom';
 import css from './styles/style.styl';
 
 import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
 
-render(<Main/>, document.getElementById('root'));
+import { Router, Route, IndexRoute, browserHistory }
+  from 'react-router';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+);
+
+render(router, document.getElementById('root'));
