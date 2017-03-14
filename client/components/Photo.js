fefcc20da2fde89a80c3b418a0ed6a7d0f6a8838
Ain <ain.salula@gmail.com>
2017-03-14 03:14:58 +0200

    photo view

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -1,5 +1,7 @@
 import React from 'React';
 import { Link } from 'react-router';
+import CSSTransitionGroup from
+  'react-addons-css-transition-group';
 
 const Photo = React.createClass({
   render() {
@@ -11,7 +13,29 @@ const Photo = React.createClass({
             <img src={post.display_src} alt={post.caption}
               className="grid-photo-wrap"/>
           </Link>
+
+          <CSSTransitionGroup transitionName="like"
+            transitionEnterTimeout={500}
+            transitionLeaveTimeout={500}>
+            <span key={post.likes} className="likes-heart">
+              {post.likes}
+            </span>
+          </CSSTransitionGroup>
         </div>
+
+        <figcaption>
+          <p>{post.caption}</p>
+          <div classnName="control-buttons">
+            <button className="likes">&hearts; {post.likes}</button>
+            <Link className="button" to={`/view/${post.code}`}>
+              <span className="comment-count">
+                <span className="speech-bubble"></span>
+                {comments[post.code] ?
+                comments[post.code].length : 0}
+              </span>
+            </Link>
+          </div>
+        </figcaption>
       </figure>
     );
   }
