93fbda6b0f416f72ba9ba305142c0df7b2e86a5d
Ain <ain.salula@gmail.com>
2017-03-14 16:56:26 +0200

    increment likes reducer

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -1,4 +1,4 @@
-import React from 'React';
+import React from 'react';
 import { Link } from 'react-router';
 import CSSTransitionGroup from
   'react-addons-css-transition-group';
@@ -25,8 +25,9 @@ const Photo = React.createClass({
 
         <figcaption>
           <p>{post.caption}</p>
-          <div classnName="control-buttons">
-            <button className="likes">&hearts; {post.likes}</button>
+          <div className="control-buttons">
+            <button onClick={this.props.increment.bind(null, i)}
+              className="likes">&hearts; {post.likes}</button>
             <Link className="button" to={`/view/${post.code}`}>
               <span className="comment-count">
                 <span className="speech-bubble"></span>
